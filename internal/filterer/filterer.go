3a32fefb18d4ee4bd7c8767be71208f9ec3b93be
David Lobe <not4rent@gmail.com>
2024-04-03 18:27:11 +0200

    filter: add some logic
    start playing with req/response params

--- internal/filterer/filterer.go
+++ internal/filterer/filterer.go
@@ -2,11 +2,15 @@ package filterer
 
 import (
 	"context"
+	"fmt"
 	"net/http"
 
 	"buf.build/gen/go/lopezator/filterer/connectrpc/go/lopezator/filterer/v1/filtererv1connect"
 	filtererpb "buf.build/gen/go/lopezator/filterer/protocolbuffers/go/lopezator/filterer/v1"
 	"connectrpc.com/connect"
+	"github.com/cockscomb/cel2sql"
+	"github.com/cockscomb/cel2sql/sqltypes"
+	"github.com/google/cel-go/cel"
 )
 
 // Service is the filterer service implementation.
@@ -20,8 +24,25 @@ func NewService() (string, http.Handler) {
 }
 
 // Filter implements filterer.FiltererServiceServer.Filter.
-func (s *Service) Filter(context.Context, *connect.Request[filtererpb.FilterRequest]) (*connect.Response[filtererpb.FilterResponse], error) {
+func (s *Service) Filter(ctx context.Context, req *connect.Request[filtererpb.FilterRequest]) (*connect.Response[filtererpb.FilterResponse], error) {
+	// TODO(lopezator): all this cel logic should be hidden somewhere else.
+	var variables []cel.EnvOption
+	for _, column := range req.Msg.Columns {
+		var columnType *cel.Type
+		switch column.Type {
+		case filtererpb.FilterRequest_Column_TYPE_STRING:
+			columnType = cel.StringType
+		}
+		variables = append(variables, cel.Variable(column.Name, columnType))
+	}
+	env, _ := cel.NewEnv(append([]cel.EnvOption{sqltypes.SQLTypeDeclarations}, variables...)...)
+	ast, _ := env.Compile(req.Msg.Expr)
+
+	// TODO(lopezator): consider using my own implementation instead.
+	where, _ := cel2sql.Convert(ast)
+
+	// Return response.
 	return connect.NewResponse(&filtererpb.FilterResponse{
-		Sql: "select * from filterer",
+		Where: fmt.Sprintf("%q", where),
 	}), nil
 }
